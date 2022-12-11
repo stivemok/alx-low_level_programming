e25f543849c5aa86532adae5620b41e1577ee465
stivemok <stivemok@gmail.com>
2022-12-11 10:26:02 -0800

    print combo4

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -6,15 +6,15 @@
  *
  * Return: Always 0.
  */
+
 int main(void)
 {
 int digit1, digit2, digit3;
-
 for (digit1 = 0; digit1 < 8; digit1++)
 {
 for (digit2 = digit1 + 1; digit2 < 9; digit2++)
-	{
-	for (digit3 = digit2 + 1; digit3 < 10; digit3++)
+{
+for (digit3 = digit2 + 1; digit3 < 10; digit3++)
 {
 putchar((digit1 % 10) + '0');
 putchar((digit2 % 10) + '0');
@@ -26,7 +26,7 @@ continue;
 putchar(',');
 putchar(' ');
 }
-	}
+}
 }
 
 putchar('\n');
