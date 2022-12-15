77e197fb29fd86483c117f0506caaab2776aa3d1
stivemok <stivemok@gmail.com>
2022-12-15 03:27:34 -0800

    leap year

--- 0x03-debugging/3-main_b.c
+++ 0x03-debugging/3-main_b.c
@@ -2,19 +2,20 @@
 #include "main.h"
 
 /**
- * main - take a date and prints how many days are left in the year, taking
+ * main - takes a date and prints how many days are left in the year, taking
  * leap years into account
  * Return: 0
  */
+
 int main(void)
 {
 	int month;
 	int day;
 	int year;
 
-	month = 4;
-	day = 01;
-	year = 1997;
+	month = 2;
+	day = 29;
+	year = 2000;
 
 	printf("Date: %02d/%02d/%04d\n", month, day, year);
 
