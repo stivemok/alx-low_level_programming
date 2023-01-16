ce4548a357e7b9d740b455a5e84e4df2cefb2c75
stivemok <stivemok@gmail.com>
2023-01-16 02:13:12 -0800

    dog

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,5 +1,6 @@
 #ifndef DOG_H
-#define Dog_H
+#define DOG_H
+
 /**
  * struct dog - dog data
  * @name: name of the dog
@@ -14,10 +15,13 @@ typedef struct dog
 	float age;
 	char *owner;
 } dog_t;
+
 /* Additional function prototypes */
+
 int _putchar(char c);
 void int_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
 void free_dog(dog_t *d);
+
 #endif
