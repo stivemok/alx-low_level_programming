90c474e55c1e04df538f5c5885cc0671060ab0d5
stivemok <stivemok@gmail.com>
2023-01-26 02:43:04 -0800

    print list

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -2,7 +2,7 @@
 #define LISTS_H
 #include <stdlib.h>
 /**
- * struct lists_s -singly linked list
+ * struct list_s -singly linked list
  * @str: string - (malloc'ed string)
  * @len: length of string
  * @next: points to the next node
