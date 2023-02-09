7d657201bbc06fe98e7dd73e7a2136137dc47c3c
stivemok <stivemok@gmail.com>
2023-02-09 01:03:00 -0800

    append text

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -1,35 +1,52 @@
 #include <stdio.h>
 #include <unistd.h>
 #include <fcntl.h>
-#include <sys/types.h>
-#include <sys/stat.h>
+
 /**
- * append_text_to_file - creates a file and puts text in it
- * with 600 perms (do not change if it exists)
+ * _strlen - compute the length of a NULL-terminated string
+ * @str: the string to measure
  *
- * @filename: name for file
- * @text_content: text to put into file
+ * Return: the length of str, or -1 if str is NULL
+ */
+ssize_t _strlen(const char *str)
+{
+	ssize_t len = 0;
+
+	if (!str)
+		return (-1);
+
+	while (*str++)
+		++len;
+
+	return (len);
+}
+
+/**
+ * append_text_to_file - append text to the end of a file
+ * @filename: the name of the file to append to
+ * @text_content: the data to append to filename
  *
- * Return: 1 on success, -1 on failure
+ * Return: Upon success, return 1. Otherwise, return -1.
  */
 int append_text_to_file(const char *filename, char *text_content)
 {
-	int file;
-	ssize_t length, inlen;
-	char *ptr;
+	ssize_t b_written = 0;
+	int fd;
 
-	if (filename == NULL)
+	if (!filename)
 		return (-1);
 
-	file = open(filename, O_WRONLY | O_APPEND);
-	if (file == -1)
+	fd = open(filename, O_WRONLY | O_APPEND);
+
+	if (fd < 0)
 		return (-1);
 
-	for (inlen = 0, ptr = text_content; *ptr; ptr++)
-		inlen++;
-	length = write(file, text_content, inlen);
+	if (text_content)
+		b_written = write(fd, text_content, _strlen(text_content));
 
-	if (close(file) == -1 || inlen != length)
+	close(fd);
+
+	if (b_written < 0)
 		return (-1);
-	return (length);
+	return (1);
 }
