7ae4c2389127819e8d7c65bd7230c43b791b8773
stivemok <stivemok@gmail.com>
2023-03-20 13:49:14 -0700

    make me win

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget /tmp/libtest.so https://github.com/stivemok/alx-low_level_programming/raw/master/0x18-dynamic_libraries/libtest.so
+wget -O /tmp/libtest.so https://github.com/stivemok/alx-low_level_programming/raw/master/0x18-dynamic_libraries/libtest.so
 export LD_PRELOAD=/tmp/libtest.so
